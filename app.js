bbb0b3d8e2234abfb42371fead2fb2cdca3e2b74
muhammadriazanus <riazanus568@gmail.com>
2023-06-05 12:33:11 -0700

    changing the social media app

--- app.js
+++ app.js
@@ -1,91 +1,102 @@
-const recipientName = document.getElementById('recipient-name');
-const recipientSurName = document.getElementById("recipient-surname");
-const recipientMobile = document.getElementById("recipient-mobile");
-const Email = document.getElementById("recipient-Email");
+const firstName = document.getElementById("recipient-name");
+const surName = document.getElementById("recipient-surname");
+const passwordsignUp = document.querySelector("#recipient-password");
+const signupEmail = document.querySelector("#recipient-Email");
+const signup = document.querySelector("#signup")
+// console.log(signup);
+//  mari login ki functionality chal rahi ha ya sa
 
+const loginEmail = document.querySelector("#email");
 
-// console.log(Email)
-// const email = document.getElementById('')
-const signUp = document.getElementById("signup")
+const loginPassword = document.querySelector("#inputPassword");
 
-// login form
-const loginEmail = document.getElementById('email')
-// console.log(loginEmail);
-const loginPassword = document.getElementById('inputPassword')
-// console.log(loginPassword);
-// console.log(login);
+const loginBtn = document.querySelector("#login")
+console.log(loginBtn);
 let date;
-let month;
-let year;
+let  year;
+let  month;
 let gender;
 
+const  users = JSON.parse(localStorage.getItem("users"))||[]
+console.log(users);
 
-const user = JSON.parse(localStorage.getItem('user')) || []
 
-login.addEventListener('click',loginHandler)
+loginBtn.addEventListener("click",loginHandler)
 
 function loginHandler(){
+    const UserFound = users.filter((user)=>{
+        return user.signupEmail === loginEmail.value
+    })
+    console.log(UserFound ,"user found  in login");
 
-    console.log(loginEmail.value);
+    if(!UserFound.length) return alert("this user is not registor please create your account ")
     console.log(loginPassword.value);
-
-    if(!loginEmail.value || !loginPassword.value) return alert("please enter your password")
-    const userFound = user.filter((user)=>{
-        console.log("user email is user found filter",user.Email);
-        return Email.value === loginEmail.value
-    })
-    if(!userFound.length) return alert("Email address already use ,please use another account")
-}
+    // console.log(UserFound.passwordsignUp);
 
 
+    if(UserFound[0].passwordsignUp == loginPassword.value){
+        alert("user is logging in")
 
+        localStorage.setItem('isloggedInUser',JSON.stringify(UserFound[0]))
 
-
-
-
-
-signUp.addEventListener('click',signUpHandler)
-function signUpHandler(){
-    const userFound = user.filter((users)=>{
-        return users.Email === loginEmail.value
-    })
-    if(userFound.length) return alert("email address is already  in use ")
-
-    console.log(recipientMobile);
-    if(recipientName.value !=="" && recipientSurName.value !=="" && recipientMobile.value !=="" && Email.value.length < 9) {
-    return alert("your password must be in 9 character")
-
-
-    let objuser = {
-        recipientMobile : recipientMobile.value,
-        recipientName : recipientName.value,
-        recipientPass:recipientPass.value,
-        recipientSurName:recipientSurName.value,
-        gender,
-        date : new Date(`${year}-${month}-${date}`)
+        window.location.href = "./dashboard/index.html"
     }
-    user.push(objuser)
-    localStorage.setItem('user',JSON.stringify(user))
-    console.log("uses sign up successfully")
-} else{
-    alert("jaldi jaldi fill kar loo bhi")
+    else{
+        alert("password is in correct ")
+    }
 }
-}
+
+// addeventlistner of sign up  function
+
+signup.addEventListener("click",signUpHandler)
+
+function signUpHandler(){
+    console.log(signupEmail.value)
+    if(firstName.value !== "" && surName.value !== "" && passwordsignUp.value!== "" && signupEmail.value !== "" && Date !== "undefined" && month !== "undefined" && year !== "undefined " && gender !=="undefined" ){
+        // return alert("please write the requriment of the  form")
+        if(passwordsignUp.value.length < 9) 
+            return alert("password must be cantain i 9 character ")
+
+            const objUser = {
+                firstName : firstName.value,
+                surName : surName.value,
+                passwordsignUp : passwordsignUp.value,
+                signupEmail:signupEmail.value,
+                date :new Date(`${year} ${month} ${date}`),
+                gender
+
+            }
+            users.push(objUser)
+            localStorage.setItem('users',JSON.stringify(users))
+            alert("User sign up sucessfully")
+
+            firstName.value = ""
+            surName.value = ""
+            passwordsignUp.value = ""
+            signupEmail.value = ""
+            
+        }else{
+            alert("insaan ban k sari field fill up karlo")
+
+        }
+        
+    }
+    
 
 function getDateHandler(d){
-    console.log(d ,"doing working")
+    console.log(d ,"datehandler is working ");
     date = d
-}
-function getMonthHandler(m){
-    console.log(m ,"doing working")
-    month = m
+
 }
 function getYearHandler(y){
-    console.log(y ,"doing working")
-    year = y
+    console.log(y,"year is working ");
+    year  = y
+}
+
+function getMonthHandler(m){
+    console.log(m,"month is working ");
+    month = m
 }
 function getGenderHandler(g){
-    console.log(g,"working");
-    gender = g
-
+    console.log(g,"gender is working ");
 }
